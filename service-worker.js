a1e347892cca7a311d6b5d1bf44539057b9332c8
maheshckwg <Maheshchin.lk@gmail.com>
2026-08-16 23:12:17 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='move-pilot-melbourne-clean-v4';
+const CACHE='move-pilot-melbourne-clean-v5';
 const CORE=['./','./index.html','./config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',e=>{
